6fd5c3c12cf04471325a6e94c072f8fac47c957e
Vanderlei Junior <vanderlei@estudante.ufscar.br>
2021-08-22 16:49:43 -0300

    [Java & MD] Code comments and readme update

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # la-lexical-analyzer
 Analizador Léxico da linguagem LA feito em Java com ANTLR4 (4.9.2) para a disciplina de Construção de Compiladores - UFSCar - ENPE 2021/2
+Lexical LA language Analyzer made in Java with ANTLR4 (4.9.2) for Compiler Construction subject - UFSCar - ENPE 2020/2
+
+*Grupo composto por Vanderlei de Brito Junior, 636932*
+
+###Pré-requisitos
+```
+Java
+```
+
+###Executando
+* Caso baixe o SOURCE:
+```
+$ java -jar <la-lexical-analyzer>/target/la-lexical-analyzer-1.0-SNAPSHOT-jar-with-dependencies.jar <arquivo de entrada> <arquivo de saída>
+```
+
+* Caso baixe o RELEASE:
+```
+$ java -jar <downloads>/la-lexical-analyzer-1.0-SNAPSHOT-jar-with-dependencies.jar <arquivo de entrada> <arquivo de saída>
+```
--- src/main/java/br/ufscar/dc/compiladores/la/lexical/analyzer/main.java
+++ src/main/java/br/ufscar/dc/compiladores/la/lexical/analyzer/main.java
@@ -13,22 +13,26 @@ public class main {
         LALexer lex = new LALexer(cs);
 
         Token t = null;
-        //String tokenName;
+        //Criação e Abertura do arquivo de saída
         BufferedWriter outputFile = new BufferedWriter(new FileWriter(args[1]));
         
         while ((t=lex.nextToken()).getType() != Token.EOF) {
-            //tokenName = LALexer.VOCABULARY.getSymbolicName(t.getType());
-            
+            //Se o tipo do token for 20 (ERRO), entra no if que categoriza o erro
             if(t.getType() == 20){
                 outputFile.append("Linha " + t.getLine() + ": ");
+                //Cadeia literal não fechada
                 if(t.getText().equals("\"")){
                     outputFile.append("cadeia literal nao fechada\n");
+                //Comentário não fechado
                 } else if(t.getText().equals("{")){
                     outputFile.append("comentario nao fechado\n");
+                //Símbolo não identificado
                 } else {
                     outputFile.append(t.getText() + " - simbolo nao identificado\n");
                 }
+                //Após a impressão do erro, finaliza do WHILE
                 break;
+            //Caso nenhum erro seja encontrado, os dois modos de impressão são feitos
             } else {
                 if(t.getType() > 2 && t.getType() < 7){
                     outputFile.append("<'" + t.getText() + "'," + LALexer.VOCABULARY.getDisplayName(t.getType()) + ">\n");
@@ -37,6 +41,7 @@ public class main {
                 }
             }
         }  
+    //Fecha o arquivo, salvando as modificações
     outputFile.close();
     }
 }
